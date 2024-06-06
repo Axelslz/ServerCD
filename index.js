364a4baa1905baa2cc62fd921879733f88a7b845
Axelslz <211103@ids.upchiapas.edu.mx>
2024-06-05 21:55:56 -0600

    Initial Commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/api/v1/welcome', (req, res) => {
-    res.send('Hello, World Daniel y Martin');
+    res.send('Hola Mampos');
 });
 
 app.listen(port, () => {
